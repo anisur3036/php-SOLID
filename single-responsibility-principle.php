fcdce73f52209c02bac8a6d9163f143870ae9983
Anisur Rahman <anisurlist3@gmail.com>
2017-08-05 17:55:17 +0600

    single responsibility principle

---
+++ single-responsibility-principle.php
@@ -0,0 +1,71 @@
+<?php
+class Report
+{
+    public function getTitle()
+    {
+        return 'Report Title';
+    }
+    public function getDate()
+    {
+        return '2016-04-21';
+    }
+    public function getContents()
+    {
+        return [
+            'title' => $this->getTitle(),
+            'date' => $this->getDate(),
+        ];
+    }
+    public function formatJson()
+    {
+        return json_encode($this->getContents());
+    }
+    public function formatHTML()
+    {
+        list('title'=>$title, 'date' => $date) = $this->getContents();
+        return "<h2>$title</h2><p>$date</p>";
+    }
+}
+
+//without srp
+$report = new Report();
+//echo $report->formatJson();
+echo $report->formatHTML();
+
+// Refactored
+class Report
+{
+    public function getTitle()
+    {
+        return 'Report Title';
+    }
+    public function getDate()
+    {
+        return '2016-04-21';
+    }
+    public function getContents()
+    {
+        return [
+            'title' => $this->getTitle(),
+            'date' => $this->getDate(),
+        ];
+    }
+}
+interface ReportFormattable
+{
+    public function format(Report $report);
+}
+class JsonReportFormatter implements ReportFormattable
+{
+    public function format(Report $report)
+    {
+        return json_encode($report->getContents());
+    }
+}
+
+
+
+//with srp
+$report = new Report();
+$json = new JsonReportFormatter();
+echo $json->format($report);
